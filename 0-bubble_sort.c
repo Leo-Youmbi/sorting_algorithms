c2a3bbd318e5d8811f5e11ae9b196f84898bc910
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-07-21 16:51:51 +0100

    sorting_algorithms tasks

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,10 +12,11 @@ void bubble_sort(int *array, size_t size)
 	unsigned int i, j = 0;
 	int tmp;
 
+	if (!array || size < 2)
+		return;
+
 	for (i = 0; i < size; i++)
-	{
 		for (j = 0; j < size - i; j++)
-		{
 			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
@@ -23,6 +24,4 @@ void bubble_sort(int *array, size_t size)
 				array[j + 1] = tmp;
 				print_array(array, size);
 			}
-		}
-	}
 }
